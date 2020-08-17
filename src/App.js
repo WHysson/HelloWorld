ce23ab14c48889a8247eb04ea8a2e8663c01adcd
WHysson <whysson.droid@gmail.com>
2020-08-17 22:11:17 +0200

    добавлена маршрутизация, страница диалогов. Переработан дизайн

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
+import Dialogs from './components/Dialogs/Dialogs';
+import { Route, BrowserRouter } from 'react-router-dom';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { faCheckSquare, faHeart } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, faCheckSquare, faHeart)
 
 function App() {
+
+  let dialogs = [
+    { id: 1, name: "Oleg Sadykov" },
+    { id: 2, name: "Alexandra Sadykova" },
+    { id: 3, name: "Vladislav Stoma" },
+    { id: 4, name: "Vladislava Leontieva" },
+    { id: 5, name: "Kirill Demchenko" },
+]
+
+let messages = [
+  { id: 1, message: "Hello world" },
+  { id: 2, message: "How are you?" },
+  { id: 3, message: "What time is it now?" },
+  { id: 4, message: "Great game, thanks!" },
+  { id: 5, message: "I love you!!!" }
+]
+
+let posts = [
+  {id: 1, postText: "My first message", likesCount: "26"},
+  {id: 1, postText: "My second message", likesCount: "52"},
+  {id: 1, postText: "Another message", likesCount: "10"},
+  {id: 1, postText: "Another one message", likesCount: "15"},
+]
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="app-wrapper">
+        <Header />
+        <Navbar />
+        <div className="app-wrapper__content">
+          <Route path='/profile' render={() =>  <Profile posts={posts}/>} />
+          <Route path='/dialogs' render={() =>  <Dialogs dialogs={dialogs} messages={messages}/>} />
+        </div>
+
+
+      </div>
+    </BrowserRouter>
   );
 }
 
